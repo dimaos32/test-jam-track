ce50c8ccebd229dfefbcd9fd0ecbd5d0810a5e2f
Dima Os <dimaos@protonmail.com>
2021-06-12 16:44:46 +0300

    Добавляет функцию автозаполнения

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+const NUMBER_OF_MATCHES = 5;
+
+const RE_LETTERS = /^[A-Za-zА-Яа-яЁё]{1}$/;
+
+const DEFAULT_DICTIONARY = [
+  'light',
+  'lime',
+  'slime',
+  'ball',
+  'Linx',
+];
+
 const addDashes = (num) => {
   num = String(num);
 
@@ -13,3 +25,57 @@ const addDashes = (num) => {
 
   return res;
 };
+
+const normalizeRequest = (request) => {
+  let res = '';
+
+  Array.from(request).filter((char) => {
+    return RE_LETTERS.test(char);
+  }).forEach((char) => {
+    res += char;
+  });
+
+  return res;
+}
+
+const checkRequest = (request, dictionaryRecord) => {
+  if (request.length > dictionaryRecord.length) {
+    return false;
+  }
+
+  for (let i = 0; i < request.length; i++) {
+    if (request[i] !== dictionaryRecord[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const autocomplite = (request, dictionary = DEFAULT_DICTIONARY) => {
+  let res = [];
+
+  if (!request.length) {
+    return res;
+  }
+
+  normalizedRequest = normalizeRequest(request)
+
+  for (let record of dictionary) {
+    if (checkRequest(normalizedRequest.toLowerCase(), record.toLowerCase())) {
+      res.push(record);
+
+      if (res.length >= NUMBER_OF_MATCHES) {
+        break;
+      }
+    }
+  }
+
+  if (!res.length) {
+    return autocomplite(request.slice(0, -1), dictionary);
+  }
+
+  return res;
+};
+
+console.log(autocomplite('lira', DEFAULT_DICTIONARY));
